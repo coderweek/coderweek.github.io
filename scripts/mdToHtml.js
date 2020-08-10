2334bbdaee0e3072e7bd2dce88ed71049d4f4b0d
jimmiezhang <jimmiezhang@tencent.com>
2020-08-10 19:29:15 +0800

    样式

--- scripts/mdToHtml.js
+++ scripts/mdToHtml.js
@@ -6,19 +6,23 @@ const html = marked(fs.readFileSync(source,'utf8'));
 
 let page = `
 <!DOCTYPE html>
-<head>`;
+<head>
+<link rel="stylesheet" href="../../style/common.css">`;
 if(process.argv[2] === 'tech'){
     page += `
 <link rel="stylesheet" href="../../style/monokai/monokai.min.css">
+<link rel="stylesheet" href="../../style/tech/index.css">
 <script src="../../lib/highlightjs/highlight.min.js"></script>
 `;
 };
 page += `
 </head>
 <body>
+<div class = "pageContainer">
 `;
 
 page += html;
+page += '</div>'
 page += `
 <script src="https://utteranc.es/client.js"
         repo="coderweek/coderweek.github.io"
